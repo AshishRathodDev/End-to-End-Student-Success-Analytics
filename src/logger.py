8fe46cb3605cc5dd0a49a4f3f482acfe4a30d0f1
AshishRathodDev <ashish3110rathod@gmail.com>
2025-06-12 00:03:20 +0530

    EDA and Problems statement

--- src/logger.py
+++ src/logger.py
@@ -2,11 +2,14 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE =f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+from networkx import has_path
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+LOG_FILE =f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_folder=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_folder,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_folder,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
@@ -16,3 +19,5 @@ logging.basicConfig(
     
 ) 
 
+if __name__=="__main__":
+    logging.info("Logging has started")
