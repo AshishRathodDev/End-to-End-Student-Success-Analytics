c99b6576ce0fdd2718d9d271b65e2d2103bb3761
AshishRathodDev <ashish3110rathod@gmail.com>
2025-06-30 13:04:00 +0530

    fix: FINAL - Changed save path to match loading path

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -24,7 +24,7 @@ from src.utils import save_object, evaluate_models
 @dataclass
 
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join('artifacts', 'model.pkl')
+    trained_model_file_path = os.path.join('src', 'model', 'model.pkl')
     
 class ModelTrainer:
     def __init__(self):    
